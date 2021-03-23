eb2887cedf774d829723d117abae5cdcdf2a0f44
jingwu <jingwu@vip.163.com>
2021-03-23 10:48:25 +0800

    任务推送重试次数可以设置

--- src/ydrabbitmq/YdRabbitMq.php
+++ src/ydrabbitmq/YdRabbitMq.php
@@ -31,14 +31,16 @@ class YdRabbitMq {
     protected        $options;
     protected        $connection         = null;
     protected        $channel            = null;
-    protected        $queueName          = '';   //队列名称
-    protected        $exchange           = '';    //交换机名称
-    protected        $routeKey           = '';    //路由名称
+    protected        $queueName          = '';      //队列名称
+    protected        $exchange           = '';      //交换机名称
+    protected        $routeKey           = '';      //路由名称
     protected static $logger             = null;
     protected static $connectionAttempts = 0;
     protected static $logDebug           = false;
     static public    $conns              = [];
     static public    $channels           = [];
+    //发送任务时的重试次数, 默认100, 同时每次重试会休眠100毫秒，最终默认异常休眠总时间为 $replyTotalPublish * 100 / 1000 = 10 秒
+    static public    $replyTotalPublish  = 100;
 
     protected      $defaultsConfig  = [
         'host'     => '127.0.0.1',
@@ -79,6 +81,14 @@ class YdRabbitMq {
 
     public static function md5sum($params) {
         return md5(json_encode($params));
+    }
+
+    //发送任务时的重试次数, 默认100, 同时每次重试会休眠100毫秒，最终默认异常休眠总时间为 $replyTotalPublish * 100 / 1000 = 10 秒
+    public static function setReplayTotalPublish($replyTotalPublish = 100) {
+        self::$replyTotalPublish = $replyTotalPublish;
+    }
+    public static function getReplayTotalPublish() {
+        return self::$replyTotalPublish;
     }
 
     public static function connect($config, $options) {
@@ -180,9 +190,8 @@ class YdRabbitMq {
         }
         //默认设置重试200次，每次休眠100毫秒
 
-        $replyTotal = 200;
-        $usleep = 100 * 1000;
-        $i = $replyTotal;
+        $usleep = 100 * 1000;   //每次重试，休眠100毫秒
+        $i = self::$replyTotalPublish;
         $flag    = true;
         while($i--) {
             try {
@@ -195,7 +204,7 @@ class YdRabbitMq {
             } catch (\PhpAmqpLib\Exception\AMQPConnectionClosedException $e) {
                 $flag = false;
                 //重试，最后一次抛异常
-                self::logInfo("RabbitMQ发送数据失败，共重试 {$replyTotal} 次，已重试 {$i} 次，exchange[{$this->exchange}] route[{$this->routeKey}] queue[{$this->queueName}] body: ".json_encode($data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES)."   AMQPConnectionClosedException: ".$e->getMessage()."    trace: ".$e->getTraceAsString());
+                self::logInfo("RabbitMQ发送数据失败，共重试 {self::$replyTotalPublish} 次，已重试 {$i} 次，exchange[{$this->exchange}] route[{$this->routeKey}] queue[{$this->queueName}] body: ".json_encode($data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES)."   AMQPConnectionClosedException: ".$e->getMessage()."    trace: ".$e->getTraceAsString());
                 usleep($usleep);
                 if($i > 0) {
                     continue;
@@ -204,7 +213,7 @@ class YdRabbitMq {
                 throw $e;
             } catch (\Exception $e) {
                 $flag = false;
-                self::logInfo("RabbitMQ发送数据失败，共重试 {$replyTotal} 次，已重试 {$i} 次，exchange[{$this->exchange}] route[{$this->routeKey}] queue[{$this->queueName}] body: ".json_encode($data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES)."   Exception: ".$e->getMessage()."    trace: ".$e->getTraceAsString());
+                self::logInfo("RabbitMQ发送数据失败，共重试 {self::$replyTotalPublish} 次，已重试 {$i} 次，exchange[{$this->exchange}] route[{$this->routeKey}] queue[{$this->queueName}] body: ".json_encode($data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES)."   Exception: ".$e->getMessage()."    trace: ".$e->getTraceAsString());
                 $connMd5Key = self::md5sum($this->config);
                 $channelMd5Key = self::md5sum([$this->config, $this->queueName, ['use_type' => $channelUseType]]);
                 //连接正常时，抛出异常
